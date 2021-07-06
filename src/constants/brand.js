01cef5ab3dc00e9dd46193d316792f2211c95af5
Abdi Daraje <adkstar10@gmail.com>
2021-07-06 08:38:23 +0300

    added branding colors and use it on chakra-ui

---
+++ src/constants/brand.js
@@ -0,0 +1,22 @@
+export const BRAND_COLORS = {
+  colors: {
+    brand: {
+      primary: "#003238",
+      secondary: "#ff8615",
+      darkTeal: "#001315",
+      darkTealSecondary: "#001f24",
+      tigerhallTeal: "#003238",
+      lightTeal: "#075e69",
+      grayTeal: "#2e4d50",
+      lightGrayTeal: "#35595d",
+      highlightGrayTeal: "#5d8180",
+      whiteTeal: "#bdd4d6",
+      popTeal: "#00f4c5",
+      tigerhallOrange: "#ff8615",
+      paleOrange: "#ff8615",
+      black: "#000000",
+      lightBlack: "#333333",
+      gray: "#989898",
+    },
+  },
+};
--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import "./index.css";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BRAND_COLORS } from "./constants/brand";
 
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const theme = extendTheme({ colors });
+const theme = extendTheme(BRAND_COLORS);
 
 ReactDOM.render(
   <React.StrictMode>
