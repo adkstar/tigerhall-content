f7083a1b68b9871361dfc0872ac1c3c4e1f6faa5
Abdi Daraje <adkstar10@gmail.com>
2021-07-06 07:54:10 +0300

    - added eslint for linting - added chakra-ui for ui components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, Stack } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Stack direction="row" spacing={4}>
+          <Button colorScheme="teal" variant="solid">
+            Email
+          </Button>
+          <Button colorScheme="teal" variant="outline">
+            Call us
+          </Button>
+        </Stack>
       </header>
     </div>
   );
